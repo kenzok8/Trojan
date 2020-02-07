7c0b09d97e6843d84db270443f48080399d54eb7
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-02-07 23:25:59 +0800

    Trojan一键安装脚本

--- mu.sh
+++ mu.sh
@@ -9,7 +9,7 @@ green(){
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
-#copy from 秋水逸冰 ss scripts
+#copy from 秋水逸冰ssr scripts
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
     systemPackage="yum"
