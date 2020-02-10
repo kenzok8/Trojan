24c55f2ad1f272ab87140340f2d40d2c4f40c187
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-02-10 22:49:36 +0800

    atrandys脚本+BBRPLUS加速

--- mult.sh
+++ mult.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-blue(){
-    echo -e "\033[34m\033[01m$1\033[0m"
+#fonts color
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
 }
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
 }
-red(){
+palegreen(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
-#copy from 秋水逸冰ssr scripts
-if [[ -f /etc/redhat-release ]]; then
+
+#copy from 秋水逸冰 ss scripts
+if [[ -f /etc/palegreenhat-release ]]; then
     release="centos"
     systemPackage="yum"
     systempwd="/usr/lib/systemd/system/"
@@ -22,7 +24,7 @@ elif cat /etc/issue | grep -Eqi "ubuntu"; then
     release="ubuntu"
     systemPackage="apt-get"
     systempwd="/lib/systemd/system/"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+elif cat /etc/issue | grep -Eqi "centos|palegreen hat|palegreenhat"; then
     release="centos"
     systemPackage="yum"
     systempwd="/usr/lib/systemd/system/"
@@ -34,35 +36,18 @@ elif cat /proc/version | grep -Eqi "ubuntu"; then
     release="ubuntu"
     systemPackage="apt-get"
     systempwd="/lib/systemd/system/"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+elif cat /proc/version | grep -Eqi "centos|palegreen hat|palegreenhat"; then
     release="centos"
     systemPackage="yum"
     systempwd="/usr/lib/systemd/system/"
 fi
 
 function install_trojan(){
-$systemPackage -y install net-tools
-Port80=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 80`
-Port443=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 443`
-if [ -n "$Port80" ]; then
-    process80=`netstat -tlpn | awk -F '[: ]+' '$5=="80"{print $9}'`
-    red "==========================================================="
-    red "检测到80端口被占用，占用进程为：${process80}，本次安装结束"
-    red "==========================================================="
-    exit 1
-fi
-if [ -n "$Port443" ]; then
-    process443=`netstat -tlpn | awk -F '[: ]+' '$5=="443"{print $9}'`
-    red "============================================================="
-    red "检测到443端口被占用，占用进程为：${process443}，本次安装结束"
-    red "============================================================="
-    exit 1
-fi
 CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
 if [ "$CHECK" == "SELINUX=enforcing" ]; then
-    red "======================================================================="
-    red "检测到SELinux为开启状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
-    red "======================================================================="
+    palegreen "======================================================================="
+    palegreen "检测到SELinux为开启状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    palegreen "======================================================================="
     read -p "是否现在重启 ?请输入 [Y/n] :" yn
 	[ -z "${yn}" ] && yn="y"
 	if [[ $yn == [Yy] ]]; then
@@ -74,9 +59,9 @@ if [ "$CHECK" == "SELINUX=enforcing" ]; then
     exit
 fi
 if [ "$CHECK" == "SELINUX=permissive" ]; then
-    red "======================================================================="
-    red "检测到SELinux为宽容状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
-    red "======================================================================="
+    palegreen "======================================================================="
+    palegreen "检测到SELinux为宽容状态，为防止申请证书失败，请先重启VPS后，再执行本脚本"
+    palegreen "======================================================================="
     read -p "是否现在重启 ?请输入 [Y/n] :" yn
 	[ -z "${yn}" ] && yn="y"
 	if [[ $yn == [Yy] ]]; then
@@ -88,16 +73,16 @@ if [ "$CHECK" == "SELINUX=permissive" ]; then
     exit
 fi
 if [ "$release" == "centos" ]; then
-    if  [ -n "$(grep ' 6\.' /etc/redhat-release)" ] ;then
-    red "==============="
-    red "当前系统不受支持"
-    red "==============="
+    if  [ -n "$(grep ' 6\.' /etc/palegreenhat-release)" ] ;then
+    palegreen "==============="
+    palegreen "当前系统不受支持"
+    palegreen "==============="
     exit
     fi
-    if  [ -n "$(grep ' 5\.' /etc/redhat-release)" ] ;then
-    red "==============="
-    red "当前系统不受支持"
-    red "==============="
+    if  [ -n "$(grep ' 5\.' /etc/palegreenhat-release)" ] ;then
+    palegreen "==============="
+    palegreen "当前系统不受支持"
+    palegreen "==============="
     exit
     fi
     systemctl stop firewalld
@@ -105,15 +90,15 @@ if [ "$release" == "centos" ]; then
     rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
 elif [ "$release" == "ubuntu" ]; then
     if  [ -n "$(grep ' 14\.' /etc/os-release)" ] ;then
-    red "==============="
-    red "当前系统不受支持"
-    red "==============="
+    palegreen "==============="
+    palegreen "当前系统不受支持"
+    palegreen "==============="
     exit
     fi
     if  [ -n "$(grep ' 12\.' /etc/os-release)" ] ;then
-    red "==============="
-    red "当前系统不受支持"
-    red "==============="
+    palegreen "==============="
+    palegreen "当前系统不受支持"
+    palegreen "==============="
     exit
     fi
     systemctl stop ufw
@@ -121,10 +106,9 @@ elif [ "$release" == "ubuntu" ]; then
     apt-get update
 fi
 $systemPackage -y install  nginx wget unzip zip curl tar >/dev/null 2>&1
-systemctl enable nginx
-systemctl stop nginx
+systemctl enable nginx.service
 green "======================="
-blue "请输入绑定到本VPS的域名"
+yellow "请输入绑定到本VPS的域名"
 green "======================="
 read your_domain
 real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
@@ -165,31 +149,26 @@ EOF
 	#设置伪装站
 	rm -rf /usr/share/nginx/html/*
 	cd /usr/share/nginx/html/
-	wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
+	wget https://github.com/V2RaySSR/Trojan/raw/master/web.zip
     	unzip web.zip
-	systemctl start nginx
-	sleep 5
+	systemctl restart nginx.service
 	#申请https证书
-	mkdir /usr/src/trojan-cert /usr/src/trojan-temp
+	mkdir /usr/src/trojan-cert
 	curl https://get.acme.sh | sh
-	~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
+	~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/
     	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
         --key-file   /usr/src/trojan-cert/private.key \
-        --fullchain-file /usr/src/trojan-cert/fullchain.cer
+        --fullchain-file /usr/src/trojan-cert/fullchain.cer \
+        --reloadcmd  "systemctl force-reload  nginx.service"
 	if test -s /usr/src/trojan-cert/fullchain.cer; then
         cd /usr/src
 	#wget https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-linux-amd64.tar.xz
-	wget https://api.github.com/repos/trojan-gfw/trojan/releases/latest
-	latest_version=`grep tag_name latest| awk -F '[:,"v]' '{print $6}'`
-	wget https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-linux-amd64.tar.xz
-	tar xf trojan-${latest_version}-linux-amd64.tar.xz
+	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+	tar xf trojan-1.*
 	#下载trojan客户端
 	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
-	wget -P /usr/src/trojan-temp https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-win.zip
 	unzip trojan-cli.zip
-	unzip /usr/src/trojan-temp/trojan-${latest_version}-win.zip -d /usr/src/trojan-temp/
 	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
-	mv -f /usr/src/trojan-temp/trojan/trojan.exe /usr/src/trojan-cli/ 
 	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
 	cat > /usr/src/trojan-cli/config.json <<-EOF
 {
@@ -297,62 +276,34 @@ EOF
 	systemctl enable trojan.service
 	green "======================================================================"
 	green "Trojan已安装完成，请使用以下链接下载trojan客户端，此客户端已配置好所有参数"
-	green "1、复制下面的链接，在浏览器打开，下载客户端，注意此下载链接将在1个小时后失效"
-	blue "http://${your_domain}/$trojan_path/trojan-cli.zip"
+	green "1、复制下面的链接，在浏览器打开，下载客户端"
+	yellow "http://${your_domain}/$trojan_path/trojan-cli.zip"
+	palegreen "请记录下面规则网址"
+	yellow "http://${your_domain}/trojan.txt"
 	green "2、将下载的压缩包解压，打开文件夹，打开start.bat即打开并运行Trojan客户端"
 	green "3、打开stop.bat即关闭Trojan客户端"
 	green "4、Trojan客户端需要搭配浏览器插件使用，例如switchyomega等"
+	green "访问  https://www.v2rayssr.com/trojan-1.html ‎ 下载 浏览器插件 及教程"
 	green "======================================================================"
 	else
-        red "==================================="
-	red "https证书没有申请成果，自动安装失败"
-	green "不要担心，你可以手动修复证书申请"
-	green "1. 重启VPS"
-	green "2. 重新执行脚本，使用修复证书功能"
-	red "==================================="
+        palegreen "================================"
+	palegreen "https证书没有申请成果，本次安装失败"
+	palegreen "================================"
 	fi
 	
 else
-	red "================================"
-	red "域名解析地址与本VPS IP地址不一致"
-	red "本次安装失败，请确保域名解析正常"
-	red "================================"
+	palegreen "================================"
+	palegreen "域名解析地址与本VPS IP地址不一致"
+	palegreen "本次安装失败，请确保域名解析正常"
+	palegreen "================================"
 fi
 }
 
-function repair_cert(){
-green "======================="
-blue "请输入绑定到本VPS的域名"
-blue "务必与之前失败使用的域名一致"
-green "======================="
-read your_domain
-real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
-local_addr=`curl ipv4.icanhazip.com`
-if [ $real_addr == $local_addr ] ; then
-    ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
-    ~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
-        --key-file   /usr/src/trojan-cert/private.key \
-        --fullchain-file /usr/src/trojan-cert/fullchain.cer
-    if test -s /usr/src/trojan-cert/fullchain.cer; then
-        green "证书申请成功"
-	green "请将/usr/src/trojan-cert/下的fullchain.cer下载放到客户端trojan-cli文件夹"
-	systemctl restart trojan
-    else
-    	red "申请证书失败"
-    fi
-else
-    red "================================"
-    red "域名解析地址与本VPS IP地址不一致"
-    red "本次安装失败，请确保域名解析正常"
-    red "================================"
-fi	
-}
-
 function remove_trojan(){
-    red "================================"
-    red "即将卸载trojan"
-    red "同时卸载安装的nginx"
-    red "================================"
+    palegreen "================================"
+    palegreen "即将卸载trojan"
+    palegreen "同时卸载安装的nginx"
+    palegreen "================================"
     systemctl stop trojan
     systemctl disable trojan
     rm -f ${systempwd}trojan.service
@@ -368,31 +319,28 @@ function remove_trojan(){
     green "=============="
 }
 
-function update_trojan(){
-    green "======================"
-    green "开发中"
-    green "======================"
-}
-
-function BBR_sh(){
-    bash <(curl -L -s -k "https://raw.githubusercontent.com/kenzok8/Trojan/master/tcp.sh")
+function bbr_boost_sh(){
+    bash <(curl -L -s -k "https://git.io/Jvc36")
 }
 
 start_menu(){
     clear
     green " ===================================="
-    green " 介绍：一键安装trojan      "
-    green " 系统：centos7+/debian9+/ubuntu16.04+"
-    green " 网站：https://github.com/kenzok8/              "
-    green " Youtube：By kenzo                "
+    green " Trojan 一键安装自动脚本          "
+    green " 系统：centos7+/debian9+/ubuntu16.04+""
+    green " atrandys脚本+BBRPLUS加速         "
+    green " Youtube：kenzo                   "
     green " ===================================="
     echo
-    green " 1. 安装trojan"
-    red " 2. 卸载trojan"
-    green " 3. 升级trojan"
-    green " 4. 修复证书"
-    yellow " 5. BBR安装"
+    palegreen " ===================================="
+    yellow " 1. 安装 Trojan"
+    palegreen " ===================================="
+    yellow " 2. 安装 BBRPLUS"
+    palegreen " ===================================="
+    yellow " 3. 卸载 Trojan"
+    palegreen " ===================================="
     yellow " 0. 退出脚本"
+    palegreen " ===================================="
     echo
     read -p "请输入数字:" num
     case "$num" in
@@ -400,23 +348,17 @@ start_menu(){
     install_trojan
     ;;
     2)
-    remove_trojan 
+    bbr_boost_sh 
     ;;
     3)
-    update_trojan 
-    ;;
-    4)
-    repair_cert 
-    ;;
-    5)
-    BBR_sh 
+    remove_trojan
     ;;
     0)
     exit 1
     ;;
     *)
     clear
-    red "请输入正确数字"
+    palegreen "请输入正确数字"
     sleep 1s
     start_menu
     ;;
