9294a5b61e8ad3e5331a8156263b11b6354c8007
kenzo <39034242+kenzok8@users.noreply.github.com>
2020-02-10 21:49:29 +0800

    Update trojan_mult.sh

--- trojan_mult.sh
+++ trojan_mult.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-blue(){
-    echo -e "\033[34m\033[01m$1\033[0m"
+#fonts color
+yellow(){
+    echo -e "\033[33m\033[01m$1\033[0m"
 }
 green(){
     echo -e "\033[32m\033[01m$1\033[0m"
@@ -9,6 +10,7 @@ green(){
 red(){
     echo -e "\033[31m\033[01m$1\033[0m"
 }
+
 #copy from 秋水逸冰 ss scripts
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
@@ -41,23 +43,6 @@ elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
 fi
 
 function install_trojan(){
-$systemPackage -y install net-tools
-Port80=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 80`
-Port443=`netstat -tlpn | awk -F '[: ]+' '$1=="tcp"{print $5}' | grep -w 443`
-if [ -n "$Port80" ]; then
-    process80=`netstat -tlpn | awk -F '[: ]+' '$5=="80"{print $9}'`
-    red "==========================================================="
-    red "检测到80端口被占用，占用进程为：${process80}，本次安装结束"
-    red "==========================================================="
-    exit 1
-fi
-if [ -n "$Port443" ]; then
-    process443=`netstat -tlpn | awk -F '[: ]+' '$5=="443"{print $9}'`
-    red "============================================================="
-    red "检测到443端口被占用，占用进程为：${process443}，本次安装结束"
-    red "============================================================="
-    exit 1
-fi
 CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
 if [ "$CHECK" == "SELINUX=enforcing" ]; then
     red "======================================================================="
@@ -121,10 +106,9 @@ elif [ "$release" == "ubuntu" ]; then
     apt-get update
 fi
 $systemPackage -y install  nginx wget unzip zip curl tar >/dev/null 2>&1
-systemctl enable nginx
-systemctl stop nginx
+systemctl enable nginx.service
 green "======================="
-blue "请输入绑定到本VPS的域名"
+yellow "请输入绑定到本VPS的域名"
 green "======================="
 read your_domain
 real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
@@ -165,31 +149,26 @@ EOF
 	#设置伪装站
 	rm -rf /usr/share/nginx/html/*
 	cd /usr/share/nginx/html/
-	wget https://github.com/atrandys/v2ray-ws-tls/raw/master/web.zip
+	wget https://github.com/V2RaySSR/Trojan/raw/master/web.zip
     	unzip web.zip
-	systemctl start nginx
-	sleep 5
+	systemctl restart nginx.service
 	#申请https证书
-	mkdir /usr/src/trojan-cert /usr/src/trojan-temp
+	mkdir /usr/src/trojan-cert
 	curl https://get.acme.sh | sh
-	~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
+	~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/
     	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
         --key-file   /usr/src/trojan-cert/private.key \
-        --fullchain-file /usr/src/trojan-cert/fullchain.cer
+        --fullchain-file /usr/src/trojan-cert/fullchain.cer \
+        --reloadcmd  "systemctl force-reload  nginx.service"
 	if test -s /usr/src/trojan-cert/fullchain.cer; then
         cd /usr/src
 	#wget https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-linux-amd64.tar.xz
-	wget https://api.github.com/repos/trojan-gfw/trojan/releases/latest
-	latest_version=`grep tag_name latest| awk -F '[:,"v]' '{print $6}'`
-	wget https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-linux-amd64.tar.xz
-	tar xf trojan-${latest_version}-linux-amd64.tar.xz
+	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
+	tar xf trojan-1.*
 	#下载trojan客户端
 	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
-	wget -P /usr/src/trojan-temp https://github.com/trojan-gfw/trojan/releases/download/v${latest_version}/trojan-${latest_version}-win.zip
 	unzip trojan-cli.zip
-	unzip /usr/src/trojan-temp/trojan-${latest_version}-win.zip -d /usr/src/trojan-temp/
 	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
-	mv -f /usr/src/trojan-temp/trojan/trojan.exe /usr/src/trojan-cli/ 
 	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
 	cat > /usr/src/trojan-cli/config.json <<-EOF
 {
@@ -297,19 +276,18 @@ EOF
 	systemctl enable trojan.service
 	green "======================================================================"
 	green "Trojan已安装完成，请使用以下链接下载trojan客户端，此客户端已配置好所有参数"
-	green "1、复制下面的链接，在浏览器打开，下载客户端，注意此下载链接将在1个小时后失效"
-	blue "http://${your_domain}/$trojan_path/trojan-cli.zip"
+	green "1、复制下面的链接，在浏览器打开，下载客户端"
+	yellow "http://${your_domain}/$trojan_path/trojan-cli.zip"
+	red "请记录下面规则网址"
+	yellow "http://${your_domain}/trojan.txt"
 	green "2、将下载的压缩包解压，打开文件夹，打开start.bat即打开并运行Trojan客户端"
 	green "3、打开stop.bat即关闭Trojan客户端"
 	green "4、Trojan客户端需要搭配浏览器插件使用，例如switchyomega等"
 	green "======================================================================"
 	else
-        red "==================================="
-	red "https证书没有申请成果，自动安装失败"
-	green "不要担心，你可以手动修复证书申请"
-	green "1. 重启VPS"
-	green "2. 重新执行脚本，使用修复证书功能"
-	red "==================================="
+        red "================================"
+	red "https证书没有申请成果，本次安装失败"
+	red "================================"
 	fi
 	
 else
@@ -318,34 +296,6 @@ else
 	red "本次安装失败，请确保域名解析正常"
 	red "================================"
 fi
-}
-
-function repair_cert(){
-green "======================="
-blue "请输入绑定到本VPS的域名"
-blue "务必与之前失败使用的域名一致"
-green "======================="
-read your_domain
-real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
-local_addr=`curl ipv4.icanhazip.com`
-if [ $real_addr == $local_addr ] ; then
-    ~/.acme.sh/acme.sh  --issue  -d $your_domain  --nginx
-    ~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
-        --key-file   /usr/src/trojan-cert/private.key \
-        --fullchain-file /usr/src/trojan-cert/fullchain.cer
-    if test -s /usr/src/trojan-cert/fullchain.cer; then
-        green "证书申请成功"
-	green "请将/usr/src/trojan-cert/下的fullchain.cer下载放到客户端trojan-cli文件夹"
-	systemctl restart trojan
-    else
-    	red "申请证书失败"
-    fi
-else
-    red "================================"
-    red "域名解析地址与本VPS IP地址不一致"
-    red "本次安装失败，请确保域名解析正常"
-    red "================================"
-fi	
 }
 
 function remove_trojan(){
@@ -367,9 +317,11 @@ function remove_trojan(){
     green "trojan删除完毕"
     green "=============="
 }
+
 function bbr_tcp_sh(){
-    bash <(curl -L -s -k "https://raw.githubusercontent.com/kenzok8/Trojan/master/tcp.sh")
+    bash <(curl -L -s -k "https://git.io/Jvc36")
 }
+
 function update_trojan(){
     green "======================"
     green "开发中"
@@ -384,15 +336,16 @@ start_menu(){
     green " By：kenzo                "
     green " ===================================="
     echo
+    red " ===================================="
     green " 1. 安装trojan"
     red " 2. 卸载trojan"
     green " 3. 升级trojan"
     yellow " 4. BBR加速"
     green " 5. 修复证书"
     blue " 0. 退出脚本"
+    red " ===================================="
     echo
     read -p "请输入数字:" num
-    case "$num" in
     1)
     install_trojan
     ;;
