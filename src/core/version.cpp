18cd4d813f5439522b2d8c52a8bfaddbc779f154
kenzok8 <kenzok8@gmail.com>
2026-03-15 22:58:18 +0800

    cleanup: remove obsolete CI/dev files and iterate v1.16.8

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -20,7 +20,7 @@
 #include "version.h"
 using namespace std;
 
-const string Version::version("1.16.7");
+const string Version::version("1.16.8");
 
 string Version::get_version() {
     return version;
